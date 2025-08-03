045c2ea6bd3723c3257e94d0d2f8658acf104aea
meAltf <alataf.dce.cs1@gmail.com>
2025-08-03 17:10:29 +0530

    Creating object using spring framework | xml beans configuration

--- src/main/java/org/learn/App.java
+++ src/main/java/org/learn/App.java
@@ -1,10 +1,18 @@
 package org.learn;
 
-public class App 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
 {
     public static void main( String[] args )
     {
-        HelloWorld helloWorld = new HelloWorld();
-        helloWorld.build();
+//        HelloWorld helloWorld = new HelloWorld();
+//        helloWorld.build();
+
+        /** Using a spring framework to create an object **/
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("springBean.xml");
+        HelloWorld helloWorldObj = applicationContext.getBean(HelloWorld.class);
+        helloWorldObj.build();
     }
 }
